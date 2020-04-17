b541622ab51e5e68cec16dab0dc43b9bff401534
github username <github email address>
2020-04-16 23:19:48 -0400

    added routes for sessions and clients controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,18 @@
 Rails.application.routes.draw do
+
+  get '/', to: 'sessions#home', as: 'root', controller: 'sessions'
+
+  get '/register', to: 'clients#new', as: 'register'
+  resources :clients, only: [:create], path: 'register'
+  resources :clients, only: [:show, :index]
+  
+  get 'login', to: 'sessions#new', as: 'login'
+  post 'login', to: 'sessions#create' 
+  delete 'logout', to: 'sessions#destroy', as: 'logout'
+
+  get '/auth/:provider/callback', to: 'sessions#google'
+
+
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
