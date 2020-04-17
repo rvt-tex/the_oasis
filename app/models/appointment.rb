c345316c0ca864b52277a04b57703a33296e89b6
github username <github email address>
2020-04-17 03:39:06 -0400

    added validation methods for appointments

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -5,15 +5,25 @@ class Appointment < ApplicationRecord
   has_many :reviews
   has_many :clients, through: :reviews
 
-  alidates :treatment, :desired_time, :desired_date, presence: true
+  validates :treatment, :desired_time, :desired_date, presence: true
 
 
   validate :desired_date_cannot_be_in_the_past
- 
-  def desired_date_cannot_be_in_thepast
+
+  def desired_date_cannot_be_in_the_past
     if desired_date.present? && desired_date < Date.today
     errors.add(:desired_date, "can't be in the past")
     end
   end 
+
+  def self.upcoming_appointments
+    appointments.order(desired_date: :desc).select { |a| a.desired_date > (DateTime.now) }
+  end
+
+  scope :alpha, -> { order(:desired_time) }
+
+  def appointment_count
+    @appointment_count ||= appointments.count
+  end
 
 end
