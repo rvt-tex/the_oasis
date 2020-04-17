6627d73a13a22cff5856c124f22fa0940af4a975
github username <github email address>
2020-04-17 00:53:36 -0400

    added helper methods, partials, and updated application layout

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,34 @@
 module ApplicationHelper
+
+    def footer_class
+        'page-footer row bg-dark text-white'
+    end
+
+    def main_col_class(text)
+        content_for :main_col_class, text
+    end
+
+    def title(text)
+        content_for :title, text
+    end
+
+    def body_container_class
+        "container mt-5"
+    end 
+
+    def card_header
+        content_tag :div, class: card_header_class do
+          yield
+        end
+    end
+
+    def card_title_class
+        "card-title mt-3 text-center"
+    end
+
+    def err
+        content_for :err do
+          yield
+        end
+    end
 end
