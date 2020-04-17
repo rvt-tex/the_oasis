f7bbe29195e2cf16ac1b0e0b3754702acae59c1e
github username <github email address>
2020-04-17 01:33:51 -0400

    added actions to treatments controller

--- app/controllers/treatments_controller.rb
+++ app/controllers/treatments_controller.rb
@@ -1,2 +1,30 @@
 class TreatmentsController < ApplicationController
+
+    def index 
+        @treatment = Treatment.all
+    end 
+
+    def new 
+        @treatment = Treatment.new  
+    end 
+
+    def create
+        @treatment = Treatment.new(treatment_params)
+        if @treatment.save 
+            redirect_to @treatment
+        else 
+            render :new 
+        end 
+    end 
+
+    def show
+        @treatment = Treatment.find(params[:id])
+    end 
+
+   
+    private 
+
+    def treatment_params
+        params.require('treatment').permit(:title, :price, :duration, :description)
+    end
 end
