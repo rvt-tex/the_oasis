f2d0ec87ca5752ea9c2d6a3c6000b6a63345f7c0
github username <github email address>
2020-04-16 23:04:09 -0400

    generated appointment model

---
+++ db/migrate/20200417030337_create_appointments.rb
@@ -0,0 +1,13 @@
+class CreateAppointments < ActiveRecord::Migration[6.0]
+  def change
+    create_table :appointments do |t|
+      t.datetime :desired_date
+      t.datetime :desired_time
+      t.string :special_request
+      t.references :client, null: false, foreign_key: true
+      t.references :treatment, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
