2a67f0982ff66eec0a33c072bfa7905568392fae
github username <github email address>
2020-04-17 07:00:28 -0400

    added class methods

--- app/models/client.rb
+++ app/models/client.rb
@@ -11,4 +11,14 @@ class Client < ApplicationRecord
     validates_numericality_of :phone, on: :create
     validates :email, format: {:with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i}, uniqueness: {message:"already belongs to an existing account"}
 
+    
+
+    def self.appointments_status
+        joins(:appointments).where("appointments.created_at < ?", Time.now)
+    end
+
+    def self.most_selected_treatment
+        joins(:appointments).group(:treatment_id).order("count(treatment_id) DESC").limit(3)
+    end
+
 end
