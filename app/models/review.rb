d43099ea57f8ec02c3d347df906094b806e92752
github username <github email address>
2020-04-17 07:34:17 -0400

    add validation for review.rb

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,4 +2,7 @@ class Review < ApplicationRecord
 
     belongs_to :client
     belongs_to :appointment
+
+    validates :comment, presence: true
+    validates :appointment, uniqueness: {scope: :client_id, message: "only one review required per appointment."}
 end
