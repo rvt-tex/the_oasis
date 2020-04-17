600ad6250bc6a406b3d84e7107f3222349c6d1d0
github username <github email address>
2020-04-17 03:30:36 -0400

    added validations to appointment model

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -5,4 +5,15 @@ class Appointment < ApplicationRecord
   has_many :reviews
   has_many :clients, through: :reviews
 
+  alidates :treatment, :desired_time, :desired_date, presence: true
+
+
+  validate :desired_date_cannot_be_in_the_past
+ 
+  def desired_date_cannot_be_in_thepast
+    if desired_date.present? && desired_date < Date.today
+    errors.add(:desired_date, "can't be in the past")
+    end
+  end 
+
 end
