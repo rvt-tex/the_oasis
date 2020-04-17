3ad1db35be8c1aacaf3876d76a1b139a6b222f39
github username <github email address>
2020-04-17 03:28:02 -0400

    added validations to client model

--- app/models/client.rb
+++ app/models/client.rb
@@ -6,4 +6,9 @@ class Client < ApplicationRecord
     has_many :appointments
     has_many :treatments, through: :appointments 
     has_many :reviewed_appointments, through: :reviews, source: :appointments 
+
+    validates_presence_of :first_name, :last_name, format: { with: /\A[a-zA-Z]+\Z/ }, message:"Cannot be blank"
+    validates_numericality_of :phone, on: :create
+    validates :email, format: {:with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i}, uniqueness: {message:"already belongs to an existing account"}
+
 end
